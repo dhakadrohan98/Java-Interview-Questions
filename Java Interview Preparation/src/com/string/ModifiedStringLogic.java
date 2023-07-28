0a500ebd10ccb9b5fc602c7141ead785fac9d77e
dhakadrohan98 <48330715+dhakadrohan98@users.noreply.github.com>
2023-07-28 23:59:46 +0530

    JIQ first 28 july
    JIQ first 28 july

---
+++ Java Interview Preparation/src/com/string/ModifiedStringLogic.java
@@ -0,0 +1,27 @@
+package com.string;
+
+public class ModifiedStringLogic {
+
+	public static void main(String[] args) {
+		// Get the String
+        String str = "Geeks Gor Geeks";
+  
+        // Get the index
+        int index = 6;
+  
+        // Get the character
+        char ch = 'F';
+  
+        // Print the original string
+        System.out.println("Original String = " + str);
+  
+        str = str.substring(0, index) + ch
+              + str.substring(index+1);
+  
+        // Print the modified string
+        System.out.println("Modified String = " + str);
+        System.out.println("Rohan"+'m');
+
+	}
+
+}
